659678e9c16efc09fb5b7634575b39b1b5a5cd92
joemcilheran <joe.mcilheran@gmail.com>
2016-05-10 13:41:35 -0500

    initial commit

---
+++ FlightToLA/FlightsFromMilwaukee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightToLA
+{
+    class FlightsFromMilwaukee
+    {
+        public FlightsFromMilwaukee()
+        {
+
+        }
+        public IEnumerator GetEnumerator()
+        {
+            string path = get_file_path("FlightList");
+            string[] flightSchedule = read_file(path);
+            foreach(string line in flightSchedule)
+            {
+                string[] lineArray = splitString(line);
+                yield return lineArray;
+            }
+
+        }
+        public string get_file_path(string fileName)
+        {
+            string path = String.Format(@"C: \Users\Joseph A McIlheran\Documents\Visual Studio 2015\Projects\FlightToLa\FlightToLa\{0}.txt", fileName);
+            return path;
+        }
+        public string[] read_file(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+            return file;
+        }
+        public string[] splitString(string input)
+        {
+            Char delimiter = ',';
+            String[] inputString = input.Split(delimiter);
+            return inputString;
+        }
+    }
+}
---
+++ FlightToLA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightToLA
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FlightsFromMilwaukee flights = new FlightsFromMilwaukee();
+            DateTime time = new DateTime(2016, 5, 10, 23, 59, 0);
+            string flightNumber = "0";
+            foreach (string[] linearray in flights)
+            {
+                if(linearray[1] == "losAngeles")
+                {
+                    DateTime flightTime = DateTime.Parse(linearray[2]);
+                    int result = DateTime.Compare(time, flightTime);
+                    if (result > 0)
+                    {
+                        time = flightTime;
+                        flightNumber = linearray[0];
+                    }
+                }
+            }
+            Console.WriteLine("flightNumber: {0}, leaving at {1}", flightNumber, time);
+            Console.ReadKey();
+        }
+    }
+}
